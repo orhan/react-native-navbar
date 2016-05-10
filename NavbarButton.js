8e81854df803526dd6c165ebb949456f3248e9bf
Orhan Sönmez <orhnsnmz@gmail.com>
2016-05-10 20:17:03 +0200

    Fixed some more import bugs

--- NavbarButton.js
+++ NavbarButton.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-const {
+import {
   Text,
   TouchableOpacity,
   View,
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-const {
+import {
   PixelRatio,
   StatusBar,
   Text,
